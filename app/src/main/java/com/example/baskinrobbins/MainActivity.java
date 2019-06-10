f44a932ee0e54881f486128d24e613ec7d32a231
yoonsua <46553255+yoonsua@users.noreply.github.com>
2019-06-10 09:24:32 +0900

    font, image

--- app/src/main/java/com/example/baskinrobbins/MainActivity.java
+++ app/src/main/java/com/example/baskinrobbins/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.baskinrobbins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViews();
+    }
+
+    private void setViews() {
+        listview = (ListView) findViewById(R.id.listview);
     }
 }
