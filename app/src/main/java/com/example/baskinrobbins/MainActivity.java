34f7ce4941ecda10f686687091eeab847d414c0e
yoonsua <46553255+yoonsua@users.noreply.github.com>
2019-06-10 09:43:58 +0900

    main incomplete

--- app/src/main/java/com/example/baskinrobbins/MainActivity.java
+++ app/src/main/java/com/example/baskinrobbins/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.baskinrobbins;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     ListView listview;
+    ListViewAdapter adapter;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            System.out.println("ㅁ");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setViews() {
         listview = (ListView) findViewById(R.id.listview);
+
+        DownThread down = new DownThread(handler);
+        down.start();
     }
 }
