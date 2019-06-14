f28f33e70cffd1a3e912298695038d74c79507e0
YoonSua <ysasm2145@naver.com>
2019-06-14 20:41:07 +0900

    Update icecreamlist

---
+++ app/src/main/java/com/example/baskinrobbins/icecreamlist.java
@@ -0,0 +1,13 @@
+package com.example.baskinrobbins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class icecreamlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icecreamlist);
+    }
+}
