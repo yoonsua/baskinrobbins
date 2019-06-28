abc62ca5c70f8c794a6b587d3eecfbadd64d75e2
yoonsua <46553255+yoonsua@users.noreply.github.com>
2019-06-28 15:01:21 +0900

    Update activity_main

--- app/src/main/java/com/example/baskinrobbins/DownThread.java
+++ app/src/main/java/com/example/baskinrobbins/DownThread.java
@@ -10,6 +10,7 @@ import org.jsoup.select.Elements;
 
 public class DownThread extends Thread {
   Handler handler;
+  private String icearr[];
   private String strurl = "http://m.baskinrobbins.co.kr/menu/list.php?top=A";
   private String starname;
 
@@ -22,49 +23,14 @@ public class DownThread extends Thread {
     while (flag) {
       try {
         Document doc = Jsoup.connect(strurl).get();
-        Elements figures = doc.getElementsByTag("figure");
-        Elements divs = doc.getElementsByTag("div");
-        flag = false;
-        for (Element figure : figures) {
-          Elements figcaptions = doc.getElementsByTag("figcaption");
-          if (figure.className().equals("img")) {
-            Elements imgs = doc.getElementsByTag("img");
-            for (Element img : imgs) {
-              String imgtxt = img.text();
-              String imgsrc = imgtxt.replace("img src = alt", "");
-              Message msg = new Message();
-              msg.what = 1;
-              msg.obj = imgsrc;
-              handler.sendMessage(msg);
-              flag = true;
-            }
-          }
-          for (Element figcaption : figcaptions) {
-            Elements spans = doc.getElementsByTag("span");
-            for (Element span : spans) {
-              Message msg = new Message();
-              msg.what = 1;
-              msg.obj = span.text();
-              handler.sendMessage(msg);
-              flag = true;
-            }
-          }
-        }
-        for(Element div : divs) {
-          if(div.className().equals("hashtag")) {
-            Elements as = doc.getElementsByTag("a");
-            for(Element a : as) {
-              Message msg = new Message();
-              msg.what = 1;
-              msg.obj = a.text();
-              handler.sendMessage(msg);
-              flag = true;
-            }
+        Elements lis = doc.getElementsByTag("li");
+        for (Element li : lis) {
+          if(li.className().equals("item")) {
+            
           }
         }
       } catch (Exception e) {
-        System.out.println("오류발생");
-        System.out.println(e.getMessage());
+        e.printStackTrace();
       }
     }
     Message msg = new Message();
--- app/src/main/java/com/example/baskinrobbins/MainActivity.java
+++ app/src/main/java/com/example/baskinrobbins/MainActivity.java
@@ -8,30 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import org.w3c.dom.Text;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageView openmenu, closemenu, menudown, img_menu;
-    LinearLayout submenu, txt_icecream, main;
-    TextView moveice;
-    ViewFlipper v_flipper;
-    ListViewAdapter adapter;
-
-    int images[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.slide4, R.drawable.slide5, R.drawable.slide6, R.drawable.slide7, R.drawable.slide8};
-
-    Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            if(msg.what == 1) {
-                adapter.addItem((String) msg.obj);
-            } else {
-                System.out.println("결과 없음");
-            }
-        }
-    };
+public class MainActivity extends AppCompatActivity {
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,69 +24,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         setViews();
-        imgslide();
-
-        adapter = new ListViewAdapter();
-
     }
 
     private void setViews() {
-        openmenu = (ImageView) findViewById(R.id.openmenu);
-        closemenu = (ImageView) findViewById(R.id.closemenu);
-        menudown = (ImageView) findViewById(R.id.menudown);
-        img_menu = (ImageView) findViewById(R.id.img_menu);
-        submenu = (LinearLayout) findViewById(R.id.submenu);
-        txt_icecream = (LinearLayout) findViewById(R.id.txt_icecream);
-        moveice = (TextView) findViewById(R.id.moveice);
-
-        openmenu.setOnClickListener(this);
-        closemenu.setOnClickListener(this);
-        menudown.setOnClickListener(this);
-        moveice.setOnClickListener(this);
-
-        DownThread thread = new DownThread(handler);
-        thread.start();
+        listview = (ListView) findViewById(R.id.listview);
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.openmenu:
-                submenu.setVisibility(View.VISIBLE);
-                break;
-            case R.id.closemenu:
-                submenu.setVisibility(View.INVISIBLE);
-                break;
-            case R.id.menudown:
-                txt_icecream.setVisibility(View.VISIBLE);
-                menudown.setImageResource(R.drawable.ico_nav_on);
-                img_menu.setImageResource(R.drawable.nav_menu_on);
-                break;
-            case R.id.moveice:
-                Intent intent = new Intent(getApplicationContext(), icecreamlist.class);
-                startActivity(intent);
-                break;
-        }
-    }
-
-    private void imgslide() {
-        v_flipper = (ViewFlipper) findViewById(R.id.image_slide);
-
-        for(int image : images) {
-            fllipperImages(image);
-        }
-    }
-
-    private void fllipperImages(int image) {
-        ImageView imageView = new ImageView(this);
-        imageView.setBackgroundResource(image);
-
-        v_flipper.addView(imageView); // 이미지 추가
-        v_flipper.setFlipInterval(4000);
-        v_flipper.setAutoStart(true); // 자동 시작
-
-        v_flipper.setInAnimation(this, android.R.anim.slide_out_right);
-        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
-    }
-
 }
